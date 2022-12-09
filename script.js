2220de0b4f9f81304c4ef2368979da43ad1c04c7
razvan232432 <marius3837@proton.me>
2022-12-09 05:40:35 -0500

    Script js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function getComputerChoice() {
 
-const pick = ['rock', 'paper', 'scissors'];
-return pick[Math.floor(Math.random() * pick.length)];
+    const pick = ['rock', 'paper', 'scissors'];
+    return pick[Math.floor(Math.random() * pick.length)];
 }
 console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+       return `It's a tie! you both picked ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+       return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+       return "You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+       return "You win! Scissors beats Paper";
+    } else {
+       return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+ }
+ 
